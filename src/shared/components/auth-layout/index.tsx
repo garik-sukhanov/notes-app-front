085a9cbd8194cac82205403d23ed5c9ff8fff3da
SukhanovIgorG <sukhanovgarik@yandex.ru>
2026-02-28 14:36:22 +0300

    feat(auth-layout): add theme-based background patterns
    Use the theme context to dynamically set the background image of the authentication layout, applying different patterns for dark and light themes

--- src/shared/components/auth-layout/index.tsx
+++ src/shared/components/auth-layout/index.tsx
@@ -1,19 +1,31 @@
 import { styled } from 'styled-components';
 
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { ThemeContext } from '@/shared/context';
+import { TOKENS } from '@/shared/tokens';
+import { ThemeVariants } from '@/shared/types/collections';
+
 export function AuthLayout() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <Wrapper>
+    <Wrapper $theme={theme}>
       <Outlet />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ $theme: ThemeVariants }>`
   height: 100dvh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  background-image: ${({ $theme }) =>
+    $theme === ThemeVariants.Dark
+      ? TOKENS.bgPatterns.secondary
+      : TOKENS.bgPatterns.primary};
 `;
